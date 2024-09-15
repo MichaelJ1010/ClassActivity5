d0ded901021c546e11d9b81bf108f2f0da9e245c
Michael J <30031129@cc.peralta.edu>
2024-09-15 14:07:57 -0700

    added size and limits for short

--- SizeAndLimit.cpp
+++ SizeAndLimit.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <iomanip>
+#include <limits>
+using namespace std;
 
 int main(){
 
+    cout << "size of a short : " << sizeof(short) << endl;
+    cout << "limits of a short (max to min): " << numeric_limits<short>::max() 
+    << " to " << numeric_limits<short>::min() << endl;
     return 0;
 }
