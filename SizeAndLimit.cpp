f2dbb78833b2c199a954010e796623afa3512e9f
Michael J <30031129@cc.peralta.edu>
2024-09-15 14:26:28 -0700

    added size and limits for a double

--- SizeAndLimit.cpp
+++ SizeAndLimit.cpp
@@ -8,5 +8,9 @@ int main(){
     cout << "size of a short : " << sizeof(short) << endl;
     cout << "limits of a short (max to min): " << numeric_limits<short>::max() 
     << " to " << numeric_limits<short>::min() << endl;
+
+    cout << "size of a double : " << sizeof(double) << endl;
+    cout << "limits of a double (max to min): " << numeric_limits<double>::max() 
+    << " to " << numeric_limits<double>::min() << endl;
     return 0;
 }
